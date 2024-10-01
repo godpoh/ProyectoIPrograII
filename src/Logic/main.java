2755738c426d0249b1823d4d9e561f7dadeb01c7
user <user@DESKTOP-2OT0H92>
2024-10-01 00:37:03 -0600

    Tarea 1 Finalizada. No se valido

---
+++ src/Logic/main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Logic;
+
+import Presentation.Log_In;
+
+/**
+ *
+ * @author user
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Log_In login = new Log_In();
+        login.setVisible(true);
+    }
+    
+}

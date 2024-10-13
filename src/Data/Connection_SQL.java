4fbd2233f3baa0c7e88eb2137691f6b92d6982bf
godpoh <jeremy12troll@gmail.com>
2024-10-12 18:48:23 -0600

    basedatos

--- src/Data/Connection_SQL.java
+++ src/Data/Connection_SQL.java
@@ -4,10 +4,32 @@
  */
 package Data;
 
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Admin
  */
 public class Connection_SQL {
-    
+
+    public static Connection getConnection() throws SQLException {
+        String Connection_String = "jdbc:sqlserver://localhost:1433;"
+                + "database=TorneoFutbol;"
+                + "user=sa;"
+                + "password=popo;"
+                + "encrypt=true;trustServerCertificate=true;";
+
+        try {
+            Connection con = DriverManager.getConnection(Connection_String);
+            return con;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+            return null;
+        }
+
+    }
+
 }
